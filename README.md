8f2a74efa36d1ec975df02f3ec32ccacf25d59fc
TobezDev <83095177+TobezDev@users.noreply.github.com>
2023-03-16 09:30:15 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Repl-Auth
+
+## Available on [GitHub](https://github.com/TobezDev/repl-auth)
+
+### This repo contains a method of authenticating Repl.it accounts with a website or webserver. Sign into pages using code from this repository is at YOUR OWN RISK and I may not be held liable for any incidents concerning the safety of accounts compromised using this login
+
+###### If you have any questions, email us [here](mailto:tobezdev@lives-in-the.cloud)
--- main.py
+++ main.py
@@ -5,19 +5,16 @@ app = Flask('app')
 
 @app.route('/')
 def home():
-	username = request.headers['X-Replit-User-Name']
-
-	if username != "" and username != None:
-		image = request.headers['X-Replit-User-Profile-Image']
-		bio = request.headers['X-Replit-User-Bio']
-		roles = request.headers['X-Replit-User-Roles']
-
-		return render_template('success.html',
+    username = request.headers['X-Replit-User-Name']
+    if username != "" and username is not None:
+        image = request.headers['X-Replit-User-Profile-Image']
+        bio = request.headers['X-Replit-User-Bio']
+        roles = request.headers['X-Replit-User-Roles']
+        return render_template('success.html',
 		                       username=username,
 		                       image=image,
 		                       bio=bio,
 		                       roles=roles)
-	return render_template('home.html')
+    return render_template('home.html')
 
-
-app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=8080, debug='True')
